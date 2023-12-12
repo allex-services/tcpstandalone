edcb5dd17b40ff681a3be672e3744a7a4e4b546a
Andrija Petrovic <andrija.hers@gmail.com>
2023-12-12 17:21:22 +0100

    Updated to comply with the latest state of allex

--- users/serviceusercreator.js
+++ users/serviceusercreator.js
@@ -7,10 +7,20 @@ function createServiceUser(execlib, ParentUser, bufferlib) {
   var lib = execlib.lib,
     q = lib.q;
 
+  function FakeGate () {
+    lib.Map.call(this);
+  }
+  lib.inherit(FakeGate, lib.Map);
+  FakeGate.prototype.handleSessionEvent = function () {
+  };
+  FakeGate.prototype.sendData = function () {
+  };
+  FakeGate.prototype.communicationType = 'tcpstandalone';
+
   function ServiceUser(prophash) {
     ParentUser.call(this, prophash);
     this.needToSend = null;
-    this.gate = new lib.Map();
+    this.gate = new FakeGate();
   }
   
   ParentUser.inherit(ServiceUser, require('../methoddescriptors/serviceuser'), [/*visible state fields here*/]/*or a ctor for StateStream filter*/);
@@ -62,19 +72,24 @@ function createServiceUser(execlib, ParentUser, bufferlib) {
   };
 
   ServiceUser.prototype.onUserIntroduced = function (connection, defer, user) {
-    try {
-      console.log('user role', user.role);
-    var session = lib.uid(),
-      usersession;
+    var session = lib.uid();
+    console.log('user role', user.role);
+    lib.qlib.thenAny(
+      user.createSession(this.gate,session,connection),
+      onUserCreatedForIntroduction.bind(this, connection, session, defer),
+      console.error.bind(console, 'oopsie')
+      //defer.reject.bind(defer)
+    );
+    connection = null;
+    session = null;
+    defer = null;
+  };
+
+  function onUserCreatedForIntroduction (connection, session, defer, usersession) {
     defer.resolve(session);
-    usersession = user.createSession(connection,session,this.gate);
     this.gate.add(usersession.session,usersession);
     connection.add(usersession);
-    } catch(e) {
-      console.error(e.stack);
-      console.error(e);
-    }
-  };
+  }
 
   ServiceUser.prototype.userInvoke = function (sessionid, methodname, paramarry, defer) {
     var usersession = this.gate.get(sessionid), user;
